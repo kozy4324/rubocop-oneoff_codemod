4fc96ccaf9fa1b19c46fb3eaafb66bf64380ff64
Koji NAKAMURA <kozy4324@gmail.com>
2025-05-23 16:54:52 +0900

    first commit

---
+++ lib/rubocop/oneoff_codemod.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "oneoff_codemod/version"
+
+module Rubocop
+  module OneoffCodemod
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
