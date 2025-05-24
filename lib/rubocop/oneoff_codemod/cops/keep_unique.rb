e2154001f58a8a4588488fb4246a9dc0d7ee8532
Koji NAKAMURA <kozy4324@gmail.com>
2025-05-24 19:00:05 +0900

    keep_unique: keeps quotes if Array items are strings

--- lib/rubocop/oneoff_codemod/cops/keep_unique.rb
+++ lib/rubocop/oneoff_codemod/cops/keep_unique.rb
@@ -27,7 +27,7 @@ module RuboCop
           end
         end
 
-        def on_array(node)
+        def on_array(node) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
           if @commands[node.location.line - 1] == :keep_unique
             add_offense(node) do |corrector|
               next if part_of_ignored_node?(node)
@@ -35,7 +35,11 @@ module RuboCop
               elements = node.values
               values = elements.map(&:source)
               uniq_values = values.uniq
-              corrector.replace(node, "[#{uniq_values.join(", ")}]")
+              if node.loc.begin.source == "["
+                corrector.replace(node, "[#{uniq_values.join(", ")}]")
+              elsif node.loc.begin.source.start_with? "%w"
+                corrector.replace(node, node.loc.begin.source + uniq_values.join(" ") + node.loc.end.source)
+              end
             end
           end
 
--- spec/rubocop/cops/keep_unique_spec.rb
+++ spec/rubocop/cops/keep_unique_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.describe RuboCop::Cop::Codemod::Command, :config do
+RSpec.describe RuboCop::Cop::Codemod::Command, :config do # rubocop:disable Metrics/BlockLength
   it "registers an offense when put `@keep-unique` comment with ArrayNode" do
     expect_offense(<<~RUBY)
       # @keep-unique
@@ -12,6 +12,42 @@ RSpec.describe RuboCop::Cop::Codemod::Command, :config do
     expect_correction(<<~RUBY)
       #
       ary = [1, 2, 3, 4]
+    RUBY
+  end
+
+  it "keeps quotes if Array items are strings" do
+    expect_offense(<<~RUBY)
+      # @keep-unique
+      ^^^^^^^^^^^^^^ keep-unique
+      ary = ["a", "b", "a", "c"]
+            ^^^^^^^^^^^^^^^^^^^^ keep-unique
+    RUBY
+
+    expect_correction(<<~RUBY)
+      #
+      ary = ["a", "b", "c"]
+    RUBY
+  end
+
+  it "keeps the string-array literal notation (%w) when correcting" do
+    expect_offense(<<~RUBY)
+      # @keep-unique
+      ^^^^^^^^^^^^^^ keep-unique
+      ary1 = %w|a b a c|
+             ^^^^^^^^^^^ keep-unique
+
+      # @keep-unique
+      ^^^^^^^^^^^^^^ keep-unique
+      ary2 = %w<a b a c>
+             ^^^^^^^^^^^ keep-unique
+    RUBY
+
+    expect_correction(<<~RUBY)
+      #
+      ary1 = %w|a b c|
+
+      #
+      ary2 = %w<a b c>
     RUBY
   end
 end
