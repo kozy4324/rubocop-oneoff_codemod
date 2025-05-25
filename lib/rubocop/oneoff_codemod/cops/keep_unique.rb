5de66e03bbb8d961a60c8137a278cf651547743d
Koji NAKAMURA <kozy4324@gmail.com>
2025-05-25 15:16:04 +0900

    feat: keep_unique keeps linebreaks

--- lib/rubocop/oneoff_codemod/cops/keep_unique.rb
+++ lib/rubocop/oneoff_codemod/cops/keep_unique.rb
@@ -27,18 +27,41 @@ module RuboCop
           end
         end
 
-        def on_array(node) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+        def on_array(node) # rubocop:disable Metrics/AbcSize,Metrics/CyclomaticComplexity,Metrics/MethodLength,Metrics/PerceivedComplexity
           return unless @commands[node.location.line - 1] == :keep_unique
+          return unless node.loc.begin.source == "[" || node.loc.begin.source.start_with?("%w")
 
           add_offense(node) do |corrector|
             next if part_of_ignored_node?(node)
 
-            uniq_values = node.values.map(&:source).uniq
+            indent = " " * node.values.first.loc.column
             if node.loc.begin.source == "["
-              corrector.replace(node, "[#{uniq_values.join(", ")}]")
-            elsif node.loc.begin.source.start_with? "%w"
-              corrector.replace(node, node.loc.begin.source + uniq_values.join(" ") + node.loc.end.source)
+              separator = ", "
+              line_separator = ",\n#{indent}"
+            else
+              separator = " "
+              line_separator = "\n#{indent}"
             end
+
+            content = []
+            content << node.loc.begin.source
+            content << processed_source.raw_source[node.loc.begin.end_pos..(node.values.first.loc.expression.begin_pos - 1)] # rubocop:disable Layout/LineLength
+
+            current_line = node.values.first.loc.line
+            acc = node.values.uniq(&:source).inject([[]]) do |acc, v_node| # rubocop:disable Style/EachWithObject
+              if current_line != v_node.loc.line
+                current_line = v_node.loc.line
+                acc << []
+              end
+              acc.last << v_node.source
+              acc
+            end
+            content << acc.map { |a| a.join(separator) }.join(line_separator)
+
+            content << processed_source.raw_source[node.values.last.loc.expression.end_pos..(node.loc.end.begin_pos - 1)] # rubocop:disable Layout/LineLength
+            content << node.loc.end.source
+
+            corrector.replace(node, content)
           end
 
           ignore_node(node)
--- spec/rubocop/cops/keep_unique_spec.rb
+++ spec/rubocop/cops/keep_unique_spec.rb
@@ -50,4 +50,61 @@ RSpec.describe RuboCop::Cop::Codemod::KeepUnique, :config do # rubocop:disable M
       ary2 = %w<a b c>
     RUBY
   end
+
+  it "keeps linebreaks" do # rubocop:disable Metrics/BlockLength
+    expect_offense(<<~RUBY)
+      # @keep-unique
+      ^^^^^^^^^^^^^^ keep-unique
+      ary1 = [1,
+             ^^^ keep-unique
+              2,
+              3,
+              2,
+              4]
+
+      # @keep-unique
+      ^^^^^^^^^^^^^^ keep-unique
+      ary2 = [1, 2,
+             ^^^^^^ keep-unique
+              2, 3, 4]
+
+      # @keep-unique
+      ^^^^^^^^^^^^^^ keep-unique
+      ary3 = %w<1 2 3
+             ^^^^^^^^ keep-unique
+                3 4 5
+                6 7 8>
+
+      # @keep-unique
+      ^^^^^^^^^^^^^^ keep-unique
+      ary4 = %w|1 2
+             ^^^^^^ keep-unique
+                3 4
+                3 2
+                5 6
+                1|
+    RUBY
+
+    expect_correction(<<~RUBY)
+      #
+      ary1 = [1,
+              2,
+              3,
+              4]
+
+      #
+      ary2 = [1, 2,
+              3, 4]
+
+      #
+      ary3 = %w<1 2 3
+                4 5
+                6 7 8>
+
+      #
+      ary4 = %w|1 2
+                3 4
+                5 6|
+    RUBY
+  end
 end
