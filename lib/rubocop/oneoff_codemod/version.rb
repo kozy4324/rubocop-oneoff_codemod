c911c2ae8feaa54ece604d06b8ce67731f301763
Koji NAKAMURA <kozy4324@gmail.com>
2025-05-27 22:36:22 +0900

    v0.0.5

--- lib/rubocop/oneoff_codemod/version.rb
+++ lib/rubocop/oneoff_codemod/version.rb
@@ -2,6 +2,6 @@
 
 module RuboCop
   module OneoffCodemod
-    VERSION = "0.0.4"
+    VERSION = "0.0.5"
   end
 end
--- spec/rubocop/oneoff_codemod_spec.rb
+++ spec/rubocop/oneoff_codemod_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe RuboCop::OneoffCodemod do
   it "has a version number" do
-    expect(RuboCop::OneoffCodemod::VERSION).to be "0.0.4"
+    expect(RuboCop::OneoffCodemod::VERSION).to be "0.0.5"
   end
 end
